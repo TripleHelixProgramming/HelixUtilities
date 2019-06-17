0b60b3264a6d59f5e7eb9d5301709251c544f7cf
notmattlythgoe <notmattlythgoe@gmail.com>
2019-06-17 12:30:08 -0400

    Add ControllerMap

---
+++ src/main/java/com/team2363/utilities/ControllerMap.java
@@ -0,0 +1,47 @@
+package com.team2363.utilities;
+
+public class ControllerMap {
+	//XboxOne Joysticks
+	public static final int X_BOX_LEFT_STICK_X = 0;
+	public static final int X_BOX_LEFT_STICK_Y = 1;
+	public static final int X_BOX_LEFT_TRIGGER = 2;
+	public static final int X_BOX_RIGHT_TRIGGER = 3;
+	public static final int X_BOX_RIGHT_STICK_X = 4;
+	public static final int X_BOX_RIGHT_STICK_Y = 5;
+	
+	//XboxOne Buttons
+	public static final int X_BOX_A = 1;
+	public static final int X_BOX_B = 2;
+	public static final int X_BOX_X = 3;
+	public static final int X_BOX_Y = 4;
+	public static final int X_BOX_LB = 5;
+	public static final int X_BOX_RB = 6;
+	public static final int X_BOX_LOGO_LEFT = 7;
+	public static final int X_BOX_LOGO_RIGHT = 8;
+	public static final int X_BOX_LEFT_STICK_BUTTON = 9;
+	public static final int X_BOX_RIGHT_STICK_BUTTON = 10;
+
+	//PS4 joystick axis
+	public static final int PS4_LEFT_STICK_X = 0;
+	public static final int PS4_LEFT_STICK_Y = 1;
+	public static final int PS4_RIGHT_STICK_X = 2;
+	public static final int PS4_LEFT_TRIGGER = 3;
+	public static final int PS4_RIGHT_TRIGGER = 4;
+	public static final int PS4_RIGHT_STICK_Y = 5;
+		
+	//PS4 Buttons
+	public static final int PS4_SQUARE = 1;
+	public static final int PS4_X = 2;
+	public static final int PS4_CIRCLE = 3;
+	public static final int PS4_TRIANGLE = 4;
+	public static final int PS4_L1 = 5;
+	public static final int PS4_R1 = 6;
+	public static final int PS4_L2 = 7;
+	public static final int PS4_R2 = 8;
+	public static final int PS4_SHARE = 9;
+	public static final int PS4_OPTIONS = 10;
+	public static final int PS4_L3 = 11;
+	public static final int PS4_R3 = 12;
+	public static final int PS4_PS = 13;
+
+}
