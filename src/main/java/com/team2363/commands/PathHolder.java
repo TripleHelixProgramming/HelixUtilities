cf732d9a2b71dc9f79d60a1f9cc4fe0b20c6bd88
Matt Lythgoe <d4lythm@swisslog.com>
2019-09-17 12:41:12 -0400

    Add path follower and decorators

---
+++ src/main/java/com/team2363/commands/PathHolder.java
@@ -0,0 +1,23 @@
+package com.team2363.commands;
+
+import com.team319.trajectory.Path;
+import com.team319.trajectory.Path.SegmentValue;
+
+public class PathHolder implements TrajectoryHolder {
+    
+    private Path path;
+
+    PathHolder(Path path) {
+        this.path = path;
+    }
+
+    @Override
+    public double getValue(int index, SegmentValue value) {
+		return path.getValue(index, value);
+	}
+
+    @Override
+    public int getSegmentCount() {
+        return path.getSegmentCount();
+    }
+}
