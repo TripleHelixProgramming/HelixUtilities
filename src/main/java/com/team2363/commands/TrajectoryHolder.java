8ef6548cbeb67f705235a9d804a8db0f1eace68a
Robotics 17.2 <menchvillerobotics@gmail.com>
2019-11-12 21:36:55 -0500

    Remove pathfinder dependency and update helixfollower

--- src/main/java/com/team2363/commands/TrajectoryHolder.java
+++
@@ -1,8 +0,0 @@
-package com.team2363.commands;
-
-import com.team319.trajectory.Path.SegmentValue;
-
-interface TrajectoryHolder {
-    double getValue(int index, SegmentValue value);
-    int getSegmentCount();
-}
